548b6fe81509c2f6803bc34454ef9429253cb491
raul-vital <raul.rsv@hotmail.com>
2024-11-24 19:25:35 -0800

    constructs POST signup route, implement and remove hashed password

--- controllers/users.js
+++ controllers/users.js
@@ -4,8 +4,30 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
-router.post('/signup', (req,res) =>{
-    res.json({message: "sucesso"})
+const SALT_LENGTH = 12
+
+router.post('/signup', async (req,res) =>{
+     try{
+        const userInDb = await User.findOne({username: req.body.username, email: req.body.email})
+        if(userInDb){
+            return res.status(400).json({err: 'Username or email already taken'})
+        }
+        const user = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, SALT_LENGTH)
+        })
+        const token = jwt.sign(
+            { username: user.username, 
+              email: user.email,
+              _id: user._id
+            }, process.env.JWT_SECRET
+        )
+        res.status(200).json({ user, token})
+      
+     }catch(err){
+        res.status(400).json({err: err.message})
+     }
 })
 
 
