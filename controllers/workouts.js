ac40c5dbf2b3a38db94cc8b257ab4ec1c3dcf493
raul-vital <raul.rsv@hotmail.com>
2024-11-28 12:20:08 -0800

    create PUT update route

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -30,7 +30,8 @@ router.get('/', async (req, res) =>{
     }
 })
 
-router.get('/:workoutId', async(req,res)=>{
+// <><><> Show <><><>
+router.get('/:workoutId', async (req,res)=>{
     try{
         const workout = await Workout.findById(req.params.workoutId).populate('user')
         res.status(200).json(workout)
@@ -39,5 +40,30 @@ router.get('/:workoutId', async(req,res)=>{
         res.status(500).json(err)
     }
 })
+
+// <><><> Update <><><>
+
+router.put('/:workoutId', async (req,res)=>{
+    try{
+        const workout = await Workout.findById(req.params.workoutId)
+        if(!workout.user.equals(req.user._id)){
+            return res.status(403).send("User Not Allowed!")
+        }
+
+        const updatedWorkout = await Workout.findByIdAndUpdate(
+            req.params.workoutId,
+            req.body,
+            {new: true}
+        )
+        updatedWorkout._doc.user = req.user
+        res.status(200).json(updatedWorkout)
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json(err)
+
+    }
+})
+
 
 module.exports = router
