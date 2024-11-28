4a4b76eae30edc84a4f736bb36dbbb886322e78e
raul-vital <raul.rsv@hotmail.com>
2024-11-28 11:48:45 -0800

    create index route, populating the user and sorting for displaying recent entries first

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -7,6 +7,7 @@ const authenticate = require('../middleware/authenticate')
 // <><><> Routes <><><>
 router.use(authenticate)
 
+// <><><> Create <><><>
 router.post('/', async(req,res)=>{
     try{
         req.body.user = req.user._id
@@ -16,6 +17,16 @@ router.post('/', async(req,res)=>{
     }catch(err){
         console.log(err)
         res.status(500).json(err)    
+    }
+})
+
+// <><><> Index <><><>
+router.get('/', async (req, res) =>{
+    try{
+        const workouts = await Workout.find({}).populate('user').sort({ createdAt: 'desc'})
+    res.status(200).json(workouts)
+    }catch(err){
+        res.status(500).json(err)
     }
 })
 
